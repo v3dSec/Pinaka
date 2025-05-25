3937b225c6464834fb824b13d4ef2cf4a63648eb
Ved Prakash Gupta <vedprakashgupta008@gmail.com>
2025-05-25 20:04:19 +0530

    Create djb2.c

---
+++ examples/djb2.c
@@ -0,0 +1,48 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "shellcode.h"
+
+void decode_djb2(const int *encoded, size_t encoded_len, unsigned char *decoded_shellcode)
+{
+    unsigned long lookup_table[256];
+
+    for (int i = 0; i < 256; i++)
+    {
+        unsigned long hash = 5381;
+        hash = ((hash << 5) + hash) + (unsigned char)i; // hash * 33 + i
+        lookup_table[i] = hash;
+    }
+
+    for (size_t i = 0; i < encoded_len; i++)
+    {
+        for (int j = 0; j < 256; j++)
+        {
+            if (lookup_table[j] == (unsigned long)encoded[i])
+            {
+                decoded_shellcode[i] = (unsigned char)j;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    LPVOID shellcode = VirtualAlloc(NULL, shellcode_len, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (shellcode == NULL)
+        return 1;
+
+    decode_djb2(encoded, shellcode_len, (unsigned char *)shellcode);
+
+    printf("\nunsigned char shellcode[] = {");
+    for (unsigned int i = 0; i < shellcode_len; i++)
+    {
+        if (i % 20 == 0)
+            printf("\n\t");
+        printf("0x%02x,", ((unsigned char *)shellcode)[i]);
+    }
+    printf("\n};\n");
+
+    return 0;
+}
